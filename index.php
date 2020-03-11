32b313695c2dce5c343d1a4a84ccc996d2e6479e
Manuel <manueldipilla3@gmail.com>
2020-03-11 18:23:33 +0100

    completo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/classes/dipendenti.php';
+require_once __DIR__ . '/classes/dirigente.php';
+require_once __DIR__ . '/classes/operaio.php';
+
+$luca = new Dirigente('12345', 'luca', 'iris', 'via lava', 'cbvnusahgan', 'it77sdvsadv31e31', 3);
+var_dump($luca);
+
+try {
+    $luca->setBonus(5);
+} catch (Exception $e) {
+    echo 'errore' . $e->getMessage();
+}
+var_dump($luca);
+
+echo $luca->getBonus();
+
+echo $luca->visualizzaDipendente();
+
+
+var_dump($luca->visualizzaDipendente());
+
